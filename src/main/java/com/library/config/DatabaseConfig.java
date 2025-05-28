db7e2f90f90098c188b40b17e3aefcbfe31b376a
HuynhLeCongLap <Lapmo843@gmail.com>
2025-05-28 19:38:11 +0700

    Upload project library-management-system-DSP

--- src/main/java/com/library/config/DatabaseConfig.java
+++ src/main/java/com/library/config/DatabaseConfig.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConfig {
     private static final String URL = "jdbc:mysql://localhost:3306/library?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "01218923570Aa"; // Thay bằng mật khẩu MySQL của bạn
+    private static final String PASSWORD = "Lapmo843@"; // Thay bằng mật khẩu MySQL của bạn
 
     static {
         try {
